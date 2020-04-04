f8e2c0ad0b55e47663e553e45ceadc030e5fab36
kamitoshi <kroo.aq.74@gmail.com>
2020-04-04 09:37:43 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_template "users/new"
+    assert_no_difference "User.count" do
+      post signup_path, params:{user:{name:"",email:"user@example.com",password:"foo",password_confirmation:"bar"}}
+    end
+    assert_template "users/new"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_template "users/new"
+    assert_difference "User.count", 1 do
+      post signup_path, params:{user:{name:"Example User",email:"user@example.com",password:"password",password_confirmation:"password"}}
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+    assert_equal flash[:success], "Welcome to the RailsTutorial!"
+  end
+
+end
