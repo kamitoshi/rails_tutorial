4e9a468f65b56e8f8250213b71abab9753970ef4
kamitoshi <kroo.aq.74@gmail.com>
2020-04-07 06:45:39 +0000

    Finish user edit, update, index, and destroy actuons

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.5.7'
 
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem "faker"
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
+  before_action :correct_user, only:[:edit, :update]
+  before_action :admin_user, only:[:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -19,6 +26,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params) # update_attributesと同じ意味になる
+      flash[:success] = "Profile updated."
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
 
   private
 
@@ -26,4 +54,25 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    # before_action
+    
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please login!"
+        redirect_to login_url
+      end
+    end
+
+    # ログインしているユーザーと一致を確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
